f205e3a6b58d345ce297519bfb4bb0779819b8ac
blueballoon <blueb4lloon1337@proton.me>
2026-01-18 20:35:17 +0100

    Launcher Support For BFN
    Finished up launcher support for BFN
    Added resizing
    Added color picker and misc tab

--- Launcher/src/config/Config.hpp
+++ Launcher/src/config/Config.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <imgui.h>
 
 #include "json.hpp"
 
@@ -20,10 +21,16 @@ struct Config {
     std::string server_ip;
     std::string password;
 
-    std::string game_selected = "GW1";
+    std::string game_selected = "gw1";
+    bool launch_directly = false;
+
+    ImVec4 baseColor = ImVec4(0.25f, 0.47f, 0.32f, 1.0f);
+    ImVec4 baseHover = ImVec4(0.30f, 0.55f, 0.38f, 1.0f);
+    ImVec4 baseActive = ImVec4(0.20f, 0.40f, 0.28f, 1.0f);
 
     GameConfig gw1;
     GameConfig gw2;
+    GameConfig bfn;
 
     void set_username(const char* val) {
         username = val;
@@ -31,19 +38,27 @@ struct Config {
     }
 
     GameConfig& get_current_game() {
-        return (game_selected == "GW2") ? gw2 : gw1;
+        if (game_selected == "gw2") return gw2;
+        if (game_selected == "bfn") return bfn;
+        return gw1;
     }
 
     const GameConfig& get_current_game() const {
-        return (game_selected == "GW2") ? gw2 : gw1;
+        if (game_selected == "gw2") return gw2;
+        if (game_selected == "bfn") return bfn;
+        return gw1;
     }
 
     int get_game_selected_int() const {
-        return (game_selected == "GW2") ? 1 : 0;
+        if (game_selected == "gw2") return 1;
+        if (game_selected == "bfn") return 2;
+        return 0;
     }
 
     void set_game_selected_from_int(int val) {
-        game_selected = (val == 1) ? "GW2" : "GW1";
+        if (val == 1) game_selected = "gw2";
+        else if (val == 2) game_selected = "bfn";
+        else game_selected = "gw1";
     }
 };
 
